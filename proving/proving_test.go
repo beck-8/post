82cb98d7503c339cbed1ccc63fd925f8c8a45704
Bartosz Różański <bartek.roza@gmail.com>
2023-02-07 16:30:41 +0100

    Add few blake3 benches and AES one

--- proving/proving_test.go
+++ proving/proving_test.go
@@ -397,7 +397,10 @@ func BenchmarkNewProving(b *testing.B) {
 		prover newProver
 		size   int
 	}{
-		{"Blake3", workNewBlake, 256 * MiB},
+		{"Blake3 with d=34", workNewBlake, 256 * MiB},
+		{"Blake3 with d=34, single invocation", workNewBlakeD34BiggerOutSize, 256 * MiB},
+		{"Blake3 with d=40, single invocation", workNewBlakeD40, 256 * MiB},
+		{"AES", workNewAES, 256 * MiB},
 	}
 
 	for _, test := range tests {
--- proving/workers_v2.go
+++ proving/workers_v2.go
@@ -2,6 +2,8 @@ package proving
 
 import (
 	"context"
+	"crypto/aes"
+	"encoding/binary"
 	"log"
 	"math"
 
@@ -16,21 +18,74 @@ type IndexReporterNew interface {
 	Report(ctx context.Context, nonce uint32, idx uint64) (stop bool)
 }
 
-func workNewBlake(ctx context.Context, data <-chan *batch, reporter IndexReporterNew, ch Challenge, difficulty []byte) {
-	// Params:
-	// Blake's output size in bits
-	const m = 512
-	// dd: |d| = log2(N) - log2(B). Assumed both N and B are power of 2.
-	dd := uint(math.Log2(N) - math.Log2(B))
-	// numOuts = ceil(numNonces * |d| / m )
-	numOuts := uint8(math.Ceil(float64(numNonces*dd) / m))
+func le40(b []byte) uint64 {
+	_ = b[4]
+	return uint64(b[0]) | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24 |
+		uint64(b[4])<<32
+}
 
-	h, err := blake3.NewKeyed(ch)
-	if err != nil {
-		log.Panicf("Failed to create new blake3 Hasher: %v", err)
+// Get an uint64 that consists of 34 bits from the data slice starting from bit i.
+func le34(data []byte, i uint) uint64 {
+	if int(i) > (len(data)*8)-34 {
+		log.Panicf("index is out of range (%d > %d)", i, (len(data)*8)-34)
 	}
+	b := data[i/8 : (i/8)+5]
+	x := binary.LittleEndian.Uint32(b)
+	// Combine the two values into an uint64
+	z := uint64(x) | uint64(b[4])<<32
+	// Shift the result to the right by the remaining bits
+	z = z >> (i % 8)
+	// Return the 34 bits from the data slice
+	return z & 0x3FFFFFFFFFFFF
+}
 
-	out := make([]byte, 128)
+func workNewBlakeD40(ctx context.Context, data <-chan *batch, reporter IndexReporterNew, ch Challenge, difficulty []byte) {
+	const m = 512 // Blake's output size in bits
+	const d = 40  // padded to 40 bits
+	const dsize = d / 8
+
+	h := blake3.New()
+	out := make([]byte, dsize*numNonces)
+	difficultyVal := le40(difficulty)
+
+	for batch := range data {
+		index := batch.Index
+		labels := batch.Data
+		for len(labels) > 0 {
+			block := labels[:B]
+			labels = labels[B:]
+
+			{
+				h.Reset()
+				h.Write(ch)
+				h.Write(block)
+				h.Write([]byte{0})
+				d := h.Digest()
+				d.Read(out) // streams variable length output
+			}
+
+			for i := 0; i < numNonces; i++ {
+				// padded to 5 bytes to avoid bit arithmetic
+				if le40(out[i*dsize:]) <= difficultyVal {
+					reporter.Report(ctx, uint32(i), index)
+				}
+			}
+			index++
+		}
+		batch.Release()
+	}
+}
+
+func workNewBlake(ctx context.Context, data <-chan *batch, reporter IndexReporterNew, ch Challenge, difficulty []byte) {
+	const m = 512 // Blake's output size in bits
+	// d: |d| = log2(N) - log2(B). Assumed both N and B are power of 2.
+	const d = 34
+	// numOuts = ceil(numNonces * |d| / m )
+	numOuts := uint8(math.Ceil(float64(numNonces*d) / m))
+
+	h := blake3.New()
+	out := make([]byte, numOuts*64)
+	difficultyVal := le34(difficulty, 0)
 
 	for batch := range data {
 		index := batch.Index
@@ -41,26 +96,105 @@ func workNewBlake(ctx context.Context, data <-chan *batch, reporter IndexReporte
 
 			for i := uint8(0); i < numOuts; i++ {
 				h.Reset()
+				h.Write(ch)
 				h.Write(block)
 				h.Write([]byte{i})
 				d := h.Digest()
 				d.Read(out[i*64 : i*64+64])
 			}
 
-			// bits := bitstream.NewReader(bytes.NewReader(out))
-			// for j := 0; j < numNonces; j++ {
-			// 	_, err := bits.Read(dd)
-			// 	if err != nil {
-			// 		log.Panicf("Failed to read next %d bits of blake3 hash out: %v", dd, err)
-			// 	}
+			for j := uint(0); j < numNonces; j++ {
+				val := le34(out, j*d)
+				if val <= difficultyVal {
+					if stop := reporter.Report(ctx, uint32(j), index); stop {
+						batch.Release()
+						return
+					}
+				}
+			}
+			index++
+		}
+		batch.Release()
+	}
+}
 
-			// 	// if bytes.Compare(outBlock, difficulty) <= 0 {
-			// 	// 	if stop := reporter.Report(ctx, uint32(j), index); stop {
-			// 	// 		batch.Release()
-			// 	// 		return
-			// 	// 	}
-			// 	// }
-			// }
+func workNewBlakeD34BiggerOutSize(ctx context.Context, data <-chan *batch, reporter IndexReporterNew, ch Challenge, difficulty []byte) {
+	const m = 512 // Blake's output size in bits
+	// d: |d| = log2(N) - log2(B). Assumed both N and B are power of 2.
+	const d = 34
+	// numOuts = ceil(numNonces * |d| / m )
+	numOuts := uint8(math.Ceil(float64(numNonces*d) / m))
+
+	h := blake3.New()
+	out := make([]byte, numOuts*64)
+	difficultyVal := le34(difficulty, 0)
+
+	for batch := range data {
+		index := batch.Index
+		labels := batch.Data
+		for len(labels) > 0 {
+			block := labels[:B]
+			labels = labels[B:]
+
+			{
+				h.Reset()
+				h.Write(ch)
+				h.Write(block)
+				h.Write([]byte{0})
+				d := h.Digest()
+				d.Read(out)
+			}
+
+			for j := uint(0); j < numNonces; j++ {
+				val := le34(out, j*d)
+				if val <= difficultyVal {
+					if stop := reporter.Report(ctx, uint32(j), index); stop {
+						batch.Release()
+						return
+					}
+				}
+			}
+			index++
+		}
+		batch.Release()
+	}
+}
+
+func workNewAES(ctx context.Context, data <-chan *batch, reporter IndexReporterNew, ch Challenge, difficulty []byte) {
+	const m = 128 // AES output size in bits
+	const blockSize = m / 8
+	// d: |d| = log2(N) - log2(B). Assumed both N and B are power of 2.
+	const dd = 34
+	// numOuts = ceil(numNonces * |d| / m )
+	numOuts := uint8(math.Ceil(float64(numNonces*dd) / m))
+	difficultyVal := le34(difficulty, 0)
+
+	c, err := aes.NewCipher(ch)
+	if err != nil {
+		log.Fatal(err)
+	}
+	out := make([]byte, numOuts*blockSize)
+
+	for batch := range data {
+		index := batch.Index
+		labels := batch.Data
+		for len(labels) > 0 {
+			block := labels[:B]
+			labels = labels[B:]
+
+			for i := uint8(0); i < numOuts; i++ {
+				c.Encrypt(out[i*blockSize:(i+1)*blockSize], block)
+			}
+
+			for j := uint(0); j < numNonces; j++ {
+				val := le34(out, j*dd)
+				if val <= difficultyVal {
+					if stop := reporter.Report(ctx, uint32(j), index); stop {
+						batch.Release()
+						return
+					}
+				}
+			}
 			index++
 		}
 		batch.Release()
